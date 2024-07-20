b244ca412b91543e8d614d9aa405611e66a19abf
Mathias Myrland <mathias.myrland@appear.net>
2024-07-20 11:16:42 +0200

    added experimental code example using syntect

--- crates/tests/dominator-css-bindgen-test-ui/src/pages/docs/doc_pages/flex.rs
+++ crates/tests/dominator-css-bindgen-test-ui/src/pages/docs/doc_pages/flex.rs
@@ -1,4 +1,7 @@
 use dominator::Dom;
+use syntect::highlighting::ThemeSet;
+use syntect::html::highlighted_html_for_string;
+use syntect::parsing::SyntaxSet;
 use dwind::prelude::*;
 use dwind_macros::{dwclass};
 use crate::pages::docs::doc_pages::doc_page::doc_page_title;
@@ -11,6 +14,7 @@ pub fn flex_page() -> Dom {
         .child(doc_page_title("Flex"))
         .text("Using flex display for adaptable containers")
         .child(flex_example_1())
+        .child(flex_example_1_code())
     })
 }
 dwgenerate!("background-scratched", "background-scratched-[#2b55a2,#5a6779]");
@@ -25,14 +29,11 @@ fn flex_example_1() -> Dom {
             .children(
                 (0..3).map(|v| {
                     html!("div", {
-                        .apply_if(v == 0, |b| {
-                            dwclass!(b, "flex-none")
-                        })
                         .apply(|b| {
                             match v {
-                                0 => dwclass!(b, "w-16 bg-candlelight-900"),
-                                1 => dwclass!(b, "w-64 bg-candlelight-600"),
-                                _ => dwclass!(b, "w-32 bg-candlelight-600"),
+                                0 => dwclass!(b, "w-14 bg-candlelight-900 flex-none"),
+                                1 => dwclass!(b, "w-64 bg-candlelight-600 flex-initial"),
+                                _ => dwclass!(b, "w-32 bg-candlelight-600 flex-initial"),
                             }
                         })
                         .dwclass!("h-full")
@@ -42,6 +43,52 @@ fn flex_example_1() -> Dom {
                     })
                 })
             )
+        }))
+    })
+}
+
+fn flex_example_1_code() -> Dom {
+    html!("div", {
+        .dwclass!("rounded-lg w-full m-t-10")
+        .dwclass!("border border-color-manatee-800")
+        .child(code(r#"html!("div", {
+    .dwclass!("rounded-lg background-scratched flex gap-4 align-items-center w-full h-p-60")
+    .children(
+        (0..3).map(|v| {
+            html!("div", {
+                .apply(|b| {
+                    match v {
+                        0 => dwclass!(b, "w-14 bg-candlelight-900 flex-none"),
+                        1 => dwclass!(b, "w-64 bg-candlelight-600 flex-initial"),
+                        _ => dwclass!(b, "w-32 bg-candlelight-600 flex-initial"),
+                    }
+                })
+                .dwclass!("h-full")
+                .dwclass!("flex align-items-center justify-center")
+                .dwclass!("rounded-lg font-extrabold")
+                .text(v.to_string().as_str())
+            })
+        })
+    )
+})"#))
+    })
+}
+
+fn code(code: &str) -> Dom {
+    let syntax_set = SyntaxSet::load_defaults_newlines();
+    let sr = syntax_set.find_syntax_by_extension("rs").unwrap();
+    let themes = ThemeSet::load_defaults();
+    let theme = &themes.themes["base16-ocean.dark"];
+    let content = highlighted_html_for_string(code, &syntax_set, sr, &theme).unwrap();
+
+    html!("pre", {
+        .child(html!("code", {
+            .with_node!(element => {
+                .apply(|b| {
+                    element.set_inner_html(content.as_str());
+                    b
+                })
+            })
         }))
     })
 }
