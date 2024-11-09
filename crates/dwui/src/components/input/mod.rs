f79c6d94a957bd0fc919f327b8137800b0e40aae
Mathias Myrland <jedimemo@gmail.com>
2024-11-09 22:31:08 +0100

    add select! component

--- crates/dwui/src/components/input/mod.rs
+++ crates/dwui/src/components/input/mod.rs
@@ -1,9 +1,11 @@
 pub mod text_input;
 pub mod validation;
 pub mod slider;
+pub mod select;
 
 pub mod prelude {
     pub use super::text_input::*;
     pub use super::slider::*;
+    pub use super::select::*;
     pub use super::validation::*;
 }
